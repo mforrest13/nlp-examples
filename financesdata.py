004503852ad8daf4d3d5af8eafabf2fc1c0e1515
Michelle Forrest <45763011+mforrest13@users.noreply.github.com>
2025-02-28 09:13:34 -0400

    new commit

---
+++ financesdata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 22:25:24 2024
+
+@author: michelleforrest
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df =pd.read_csv("company_financials.csv")
+df.head()
